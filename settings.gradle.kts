bddfd2fbb8916d0020f68e94b317e49f22ef56b4
*Tayyab <*i200590@nu.edu.pk*>
2023-09-13 21:03:25 +0500

    Commit 1, On 5th Screen

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "i200590"
+include(":app")
