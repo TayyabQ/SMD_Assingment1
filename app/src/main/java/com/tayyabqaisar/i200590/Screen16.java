2f0af11903aaa317ed7b718bdac053ac016888b0
*Tayyab <*i200590@nu.edu.pk*>
2023-09-15 19:24:56 +0500

    Commit 2, 13 Screens

---
+++ app/src/main/java/com/tayyabqaisar/i200590/Screen16.java
@@ -0,0 +1,14 @@
+package com.tayyabqaisar.i200590;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Screen16 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen16);
+    }
+}
