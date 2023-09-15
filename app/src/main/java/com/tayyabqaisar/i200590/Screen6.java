8576042087ce163bddf47dcdb41c0c022010c790
*Tayyab <*i200590@nu.edu.pk*>
2023-09-15 14:52:16 +0500

    Commit 2, 6 Screens

---
+++ app/src/main/java/com/tayyabqaisar/i200590/Screen6.java
@@ -0,0 +1,16 @@
+package com.tayyabqaisar.i200590;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class Screen6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen6);
+
+    }
+}
